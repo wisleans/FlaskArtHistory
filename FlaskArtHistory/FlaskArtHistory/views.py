f4bc7732f7d468f8de0bf81adbddfb0b8758a677
wisleans <wisleans@gmail.com>
2017-05-31 21:35:02 -0700

    added

--- FlaskArtHistory/FlaskArtHistory/views.py
+++ FlaskArtHistory/FlaskArtHistory/views.py
@@ -6,15 +6,33 @@ from datetime import datetime
 from flask import render_template
 from FlaskArtHistory import app
 
+class links(list):
+    """description of class"""
+    def add_new_arrival(self, name, url):
+        self.append((False, name,url))
+
+linklist = links()
+
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
+    linklist.add_new_arrival("name","url")
     return render_template(
         'index.html',
         title='Home Page',
         year=datetime.now().year,
     )
+
+@app.route('/history')
+def history():
+    """Renders the home page."""
+    linklist.add_new_arrival("name","url")
+    return render_template(
+        'history.html',
+        title='History'
+    )
+
 
 @app.route('/contact')
 def contact():
